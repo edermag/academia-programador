df51dc0e52dc800cad382b385f06bf6305917d05
Eder Magalhães <edermag@gmail.com>
2013-07-24 23:23:48 -0300

    inversão de inteiro em Java e Python

---
+++ python/inverteNumero.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Programa Python implementa uma calculadora, com uma função para cada operação básica.
+#
+# Módulo: Lógica de programação
+# Author: Eder Magalhães
+
+numero = int(raw_input("Entre com o nũmero... "));
+print "Número informado ",numero
+
+numeroInvertido = int(str(numero)[::-1]);
+
+print "O número invertido: ",numeroInvertido
+
+

0cf8b1e10fc894bf8ec45fc0cb945bec35c76cea
BOT-Samuel <56443581+BOT-Samuel@users.noreply.github.com>
2019-10-11 12:24:30 -0300

    Update inverteNumero.py

--- python/inverteNumero.py
+++ python/inverteNumero.py
@@ -1,17 +1,11 @@
-# -*- coding: utf-8 -*-
 
-# Programa Python demonstra como inverter um número inteiro.
-#
-# Acesse e execute esse código Python em: http://pythonfiddle.com/inverte-numero-ap/
-#
-# Módulo: Lógica de programação
-# Author: Eder Magalhães
+# Author: Samuel Olimpio
 
-numero = int(raw_input("Entre com o nũmero... "));
-print "Número informado ",numero
+numero = int(input("Entre com o numero... "));
+print ("Número informado ",numero)
 
 numeroInvertido = int(str(numero)[::-1]);
 
-print "O número invertido: ",numeroInvertido
+print ("O número invertido: ",numeroInvertido)
 
-
+#Executavel em python 3.7
